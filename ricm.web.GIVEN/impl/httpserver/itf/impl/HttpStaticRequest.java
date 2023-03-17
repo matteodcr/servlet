71e4a870d646108aca84f146ed884009c4b8bcf7
Mattéo D <mattdecorsaire@gmail.com>
2023-03-17 08:34:48 +0100

    Base code

---
+++ ricm.web.GIVEN/impl/httpserver/itf/impl/HttpStaticRequest.java
@@ -0,0 +1,23 @@
+package httpserver.itf.impl;
+
+import java.io.IOException;
+
+
+import httpserver.itf.HttpRequest;
+import httpserver.itf.HttpResponse;
+
+/*
+ * This class allows to build an object representing an HTTP static request
+ */
+public class HttpStaticRequest extends HttpRequest {
+	static final String DEFAULT_FILE = "index.html";
+	
+	public HttpStaticRequest(HttpServer hs, String method, String ressname) throws IOException {
+		super(hs, method, ressname);
+	}
+	
+	public void process(HttpResponse resp) throws Exception {
+	// TO COMPLETE
+	}
+
+}
